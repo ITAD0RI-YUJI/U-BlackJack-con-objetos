79a18380f31c7d1b3309a84bd0735cccea955383
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2024-02-21 13:07:09 -0500

    Agregando función de mostrar cartas y desordenar el arreglo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class carta:
     def mostrar(self):
         return self.valor + " " + self.pinta
 
-    def darValor(self):
+    def darValor(self): 
         if self.valor in ['J' , 'Q' , 'K']:
             return 10
         if self.valor == 'A':
@@ -22,19 +22,36 @@ class mazo:
         else:
             self.cartas = [carta(v , p) for v in ['A' , 'J' , 'Q' , 'K'] + [str(x) for x in range(2 , 11)] 
                                         for p in ['♠' , '♥' , '♦' , '♣']]
+            shuffle(self.cartas)
    
     def darValor(self):
         valor = 0;
         for c in self.cartas:
             valor += c.darValor()
-        
+        if self.tienesAs() and valor <= 11:
+            valor += 10
         return valor
+
+    def tienesAs(self):
+        for c in self.cartas:
+            if c.valor == 'A':
+                return True
+        return False
 
     def darCarta(self):
         return self.cartas.pop()
 
     def agregarCarta(self , carta):
         self.cartas.append(carta)
+
+    def mostrarCartas(self , todas = False):
+        if todas:
+            print(self.cartas[0].mostrar())
+        else:
+            print("* de *")
+        
+        for c in self.cartas[1:]: # Recorre desde el elemento 2, o sea el elemento 1 no lo muestra
+            print(c.mostrar())
 
 if __name__ == '__main__':
     m = mazo()
@@ -46,6 +63,5 @@ if __name__ == '__main__':
         print(c.mostrar())
     print(j.darValor())
 
-#Cómo desordenar las cartas desde el principio?  -  TAREA 
 
 # DUDA: las variables que se crean dentro del objeto, son globales en ese objeto? (o sea todo lo que esté dentro de ese objeto puede acceder a ella?) 
