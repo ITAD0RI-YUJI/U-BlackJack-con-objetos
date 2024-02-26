1bc97097cba6726f4da13bbedd56b86478474ffd
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2024-02-25 19:28:14 -0500

    Intentando agrega la función para platarse (no funciona)

--- juego.py
+++ juego.py
@@ -5,13 +5,14 @@ class Usuario:
         self.nombre = nombre
     
     def User_presentation(self):
-        return print("\n♥ Hola ", self.nombre, " bienvenido")
+        return self.nombre
 
 class juego:
-    def __init__(self):
+    def __init__(self , plantarse = False):
         self.mazo = mazo()
         self.casa = mazo(True)
         self.jugador = mazo(True)
+        self.plantarse = plantarse
     
     def iniciarJuego(self): 
         self.casa.agregarCarta(self.mazo.darCarta())
@@ -20,8 +21,16 @@ class juego:
         self.jugador.agregarCarta(self.mazo.darCarta())
 
     def mostrarJuego(self):
-        print("\n• JUGADOR: ")  #Aprender como poner el nombre acá (tal vez en con herencia)
+        print("\n• JUGADOR: ")  #Aprender cómo poner el nombre acá (tal vez en con herencia)
         self.jugador.mostrarCartas()
 
         print("\n• CASA: ")
         self.casa.mostrarCartas()
+
+    def plantarse_function(self):
+        if self.plantarse == True:
+            print("\n• Te plantaste, tus puntos son:")
+            mazo.mostrarCartas(True)
+            mazo.darValor()
+        else:
+            pass
--- principal.py
+++ principal.py
@@ -8,3 +8,9 @@ print("\n♥ Hola ",user.User_presentation(), " bienvenido")
 juego = juego()
 juego.iniciarJuego()
 juego.mostrarJuego() 
+
+for i in range(9):
+    plantarse = input("¿Deseas agregar otra carta a tu mazo?: ")
+
+    if plantarse == "no" or plantarse == "NO":
+        juego.plantarse(True)
